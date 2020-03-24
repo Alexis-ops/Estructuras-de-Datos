5c333c7d98e72fdf090c9d6a1ec493c9c12a9e71
Alexis-ops <61523995+Alexis-ops@users.noreply.github.com>
2020-03-24 01:57:00 -0600

    Add files via upload
    colo estatica, que funciona como una cola en circulo

--- colaC.c
+++ colaC.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max 6
+#define max 5
 
 struct ColaC{
 	int inicio;
@@ -11,16 +11,25 @@ void iniciar(struct ColaC*cola);
 int vacia(struct ColaC*cola);
 int llena(struct ColaC*cola);
 int elementos(struct ColaC*cola);
-void agregar(struct ColaC*cola);
-void mostrar(struct ColaC*cola,int dato);
-void eliminar(struct ColaC*cola);
+void enqueve(struct ColaC*cola,int dato);
+void mostrar(struct ColaC*cola);
+void dequeve(struct ColaC*cola);
 int main(){
+  struct ColaC cola;
   iniciar(&cola);
+  enqueve(&cola,1);
+  enqueve(&cola,2);
+  enqueve(&cola,3);
+  enqueve(&cola,4);
+  enqueve(&cola,5);
+  dequeve(&cola);
+  printf("El inicio es: %d, El final es: %d \n", cola.array[cola.inicio],cola.array[cola.final]);
+  mostrar(&cola);
 }
 
-void(struct ColaC*cola){
+void iniciar(struct ColaC*cola){
   cola->final=-1;
-  cola->inicio=-1
+  cola->inicio=-1;
 }
 int vacia(struct ColaC*cola){
   if(cola->final == -1){
@@ -31,20 +40,22 @@ int vacia(struct ColaC*cola){
 int elementos(struct ColaC * cola){
   if(vacia(cola)==1){
     return 0;
-  }
-  if(cola->final >= cola->inicio){
-    return (cola->final)-(cola->inicio)+1;
   }else{
-    return max-(cola->inicio)+(cola->final)+1;
+    if(cola->final >= cola->inicio){
+      return (cola->final)-(cola->inicio)+1;
+    }else{
+      return max - (cola->inicio) + (cola->final) + 1;
+    }
   }
 }
 int llena(struct ColaC * cola){
   if(elementos(cola)==max){
     return 1;
+  }else{
+    return 0;
   }
-  return 0;
 }
-void agregar(struct ColaC*cola, int dato){
+void enqueve(struct ColaC*cola, int dato){
   if(llena(cola)==0){
     if(vacia(cola)==1){
       cola->inicio=0;
@@ -56,24 +67,51 @@ void agregar(struct ColaC*cola, int dato){
       cola->final++;
     }
     cola->array[cola->final]=dato;
+  }else{
+    printf("La cola esta llena, espera que se libere un espacio\n");
   }
-  printf("La cola esta llena, espera que se libere un espacio");
 }
-void eliminar(struct ColaC*cola){
-  int x=0;
+void dequeve(struct ColaC*cola){
+  int aux=0;
   if(vacia(cola)==0){
+    aux=cola->array[cola->inicio];
     if(elementos(cola)==1){
       iniciar(cola);
     }
-    else{
-      cola->inicio++;
+    if(cola->inicio == max-1){
+      cola->inicio=0;
+    }else{
+        cola->inicio++;
+        printf("El dato que se desencolo fue: %d\n",aux);
     }
-    /*if(cola->final >= cola->inicio){
-      for(x=1;x<=cola-final;x++){
-        cola->array[x-1]=cola->array[x];
+    
+  }else {
+    printf("La cola esta vacia, no hay datos que se puedan eliminar\n");
+  }
+}
+
+/*
+f       i
+0 1 2 3 4
+6       5
+
+*/
+
+void mostrar(struct ColaC*cola){
+  int x=0; 
+  if(vacia(cola) == 0){
+    if(cola->final >= cola->inicio ){
+      for(x=cola->inicio;x<=cola->final;x++){
+        printf("%d,",cola->array[x]);
       }
-      cola->final--;
+    }else{
+      for(x=cola->inicio;x<=max-1;x++){
+        printf("%d,",cola->array[x]);
+      }
+      for(x=0;x <= cola->final ; x++){}
+        printf("%d,",cola->array[x]);
     }
-  */}
-  printf("La cola esta vacia, no hay datos que se puedan eliminar");
+  }else{
+    printf("La cola esta vacia \n");
+ }
 }
