83795c91d3a481df92207feca2f801e4e0f3a357
Alexis <nemoalexis1517@gmail.com>
2020-06-03 21:35:02 -0500

    Primer intento

--- Hash-Table/hash1.c
+++ Hash-Table/hash1.c
@@ -5,73 +5,86 @@
 /* Esta estructura debería estar en un archivo .h*/
 struct Lista{
   int dato;
-  int key;
   struct Lista *siguiente;  
 };
+struct Lista * cabeza[]=NULL;
+struct Lista * c;
+void insertar(){
+	int i;
+	int key;
+	printf("ingresa un valor\n");
+	scanf("%d",&key);
+	i=key%size;
+	struct Lista* Nlista=(struct Lista*)malloc(sizeof(struct Lista));
+	Nlista->dato=key;
+	Nlista->siguiente=NULL;
+	if(cabeza[i] == NULL){
+		cabeza[i] == Nlista
+	}
+	else {
+		c=cabeza[i];
+		while(c->siguiente){
+			c=c->siguiente;
+		}
+		c->siguiente=Nlista;
+	}
+}
+void buscar(){
+	int key;
+	int indice;
+	printf("ingresa el elemento a buscar\n");
+	scanf("%d",&key);
+	indice=key%size;
+	if(head[indice]==NULL){
+		printf("Elemento no encontrado\d");
+	}else{
+		for(c=cabeza[indice];c!=NULL;c=c->siguiente){
+			if(c->dato == key){
+				printf("Elemento encontrado en el indice %d \n",indice);
+				break;
+			}
+		}
+		if(c == NULL){
+			printf("Elemento no encontrado \n");
+		}
+	}
+}
 
-/* Un nombre más adecuado es TablaHash, el 
-nombre Arreglo confunde */
-struct TablaHash{
-  int vector[size];
-};
-
-void insertar(struct Arreglo*arreglo,int dato);
-int hash(int dato);
-
-
-/* Para una tabla Hash no se puede programar la función mostrar,
-en lugar de eso se programará la función: buscar */
-void mostrar(struct Arreglo*arreglo);
-
-/*int crear(int dato);
-struct Lista * enqueve(struct Lista*lista, int dato);*/
+void mostrar(){
+	int i=0;
+	for(i=0;i<size;i++){
+		printf("\n");
+		if(cabeza[i] == NULL){
+			continue;
+		}
+		else{
+			for(c=cabeza[i];c!=NULL;c=c->siguiente){
+				printf("->%d",c->dato);
+			}
+		}
+	}
+}
 
 int main(){
-  struct Arreglo arreglo;
-  struct Lista*miLista=NULL;
-  insertar(&arreglo,16);
-  insertar(&arreglo,7);
-  insertar(&arreglo,8);
-  insertar(&arreglo,9);
-  insertar(&arreglo,10);
-  insertar(&arreglo,11);
-  insertar(&arreglo,12);
-  insertar(&arreglo,13);
-  insertar(&arreglo,14);
-  insertar(&arreglo,15);
-  mostrar(&arreglo);
+  int opt;
+  int key;
+  while (1){
+	  printf("\n presiona 1. Insertar\t 2.Mostrar\t3.buscar \t4.Salir \n ");
+	  scanf("%d",%opt);
+	  switch(opt){
+		  case 1:
+			  insertar();
+			  break;
+		  case 2:
+			  mostrar();
+			  break;
+		  case 3:
+			  buscar();
+			  break;
+		  case 4:
+			  exit(0);
+
+	  }
+  }
 }
-void insertar(struct Arreglo*arreglo,int dato){
-	struct lista*miLista=NULL;
-	  arreglo->vector[hash(dato)]=dato;
-}
-int hash(int dato){
-	return (dato+4)%size;
-}
-/*void mostrar(struct Arreglo*arreglo){
-	int x=0;
-	for(x=0;x<size;x++){
-		printf("%d\n",arreglo->vector[x]);
-	}
-}*/
-/*int crear(int dato){
-	struct Lista*nuevo=NULL;
-	nuevo=(struct Lista*)malloc(sizeof(struct Lista));
-	nuevo->dato=dato;
-	nuevo->siguiente=NULL;
-	return nuevo;
-}
-struct Lista*enqueve(struct Lista*lista, int dato){
-	struct Lista*queve=crear(dato);
-	struct Lista*recorrer=lista;
-	if(lista!=NULL){
-		while(recorrer->siguiente!=NULL){
-			recorrer=recorrer->siguiente;
-		}
-		recorrer->siguiente=queve;
-	}else{
-		queve->siguiente=lista;
-	}
-	return queve;
-}
-*/
+
