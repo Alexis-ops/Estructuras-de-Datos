1056c9ac1cc85abe11d8655626461e69ceba64e6
Alexis <nemoalexis1517@gmail.com>
2020-06-11 23:58:08 -0500

    solo insertar

---
+++ Árbol/arbol.h
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+struct Arbol
+{
+  int dato;
+  struct Arbol *izquierdo;
+  struct Arbol *derecho;
+};
+
+
+
+struct Arbol *insertar (struct Arbol *, int);
+
+
+
+
